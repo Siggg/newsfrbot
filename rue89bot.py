feb4deb60056e19358d80878ef9cfdea935b7a0d
Yves Quemener <Iv@laptop>
2012-10-08 20:38:16 +1100

    Added time restrictions and debug output

--- rue89bot.py
+++ rue89bot.py
@@ -2,6 +2,8 @@ import feedparser
 import cPickle
 import praw
 
+from time import *
+
 # TODO : improve the "database" system that will get corrupted if interrupted and take o(n) time with n=entries already published
 
 reddit = praw.Reddit(user_agent='Rue89 bot, by u/keepthepace')
@@ -9,11 +11,15 @@ reddit.login('keepthepace', 'rttrtt')
 
 already_published = cPickle.load(open("already_published","rb"))
 
-d = feedparser.parse('http://rue89.feedsportal.com/c/33822/f/608948/index.rss')
+while True:
+    d = feedparser.parse('http://rue89.feedsportal.com/c/33822/f/608948/index.rss')
 
-for e in d["entries"]:
-    if not e["link"] in already_published:
-        reddit.submit('rue89', e["title"], url=e['link'])
-        already_published.add(e["link"])
-        cPickle.dump(already_published,open("already_published","w"))
+    for e in d["entries"]:
+        if not e["link"] in already_published:
+            reddit.submit('rue89', e["title"], url=e['link'])
+            sleep(2) # To comply with reddit's policy : no more than 0.5 req/sec
+            already_published.add(e["link"])
+            cPickle.dump(already_published,open("already_published","w"))
+            print print asctime(), e["title"]
+    sleep(300)
 
