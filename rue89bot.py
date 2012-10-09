80297e6b8de0340c1b7c2da9c9dd1df85bb1408e
Yves Quemener <yves@igor.(none)>
2012-10-10 01:52:09 +0200

    Overdue commit : now correctly identify already published items and catch reddit exceptions

--- rue89bot.py
+++ rue89bot.py
@@ -2,12 +2,19 @@ import feedparser
 import cPickle
 import praw
 
+import traceback
+import sys
+import getpass
+
 from time import *
 
 # TODO : improve the "database" system that will get corrupted if interrupted and take o(n) time with n=entries already published
 
 reddit = praw.Reddit(user_agent='Rue89 bot, by u/keepthepace')
-reddit.login('keepthepace', 'rttrtt')
+user='newsfrbot'
+print "Password for",user,"?"
+passwd=getpass.getpass()
+reddit.login(user, passwd)
 
 already_published = cPickle.load(open("already_published","rb"))
 
@@ -17,13 +24,24 @@ while True:
     for e in d["entries"]:
         if not e["link"] in already_published:
             try:
+                print asctime(), "Publishing :", e["title"]
                 reddit.submit('rue89', e["title"], url=e['link'])
                 sleep(2) # To comply with reddit's policy : no more than 0.5 req/sec
                 already_published.add(e["link"])
                 cPickle.dump(already_published,open("already_published","w"))
-                print asctime(), e["title"]
-            except:
-		print "Exception : Reddit offline ? Retrying in 5 minutes"
-		sleep(300)
+            except praw.errors.APIException as ex:
+                if ex.error_type=='ALREADY_SUB':
+                    already_published.add(e["link"])
+                    print "Already published :", e["title"]
+                    cPickle.dump(already_published,open("already_published","w"))
+                else:
+                    print asctime(),"Exception : Reddit offline ? Retrying in 5 minutes"
+                    traceback.print_stack()
+                    print ex.error_type
+                    print ex.message
+                    print ex.field
+                    print ex.response
+                    print "_", sys.exc_info()[0]
+                    sleep(300)
     sleep(300)
 
