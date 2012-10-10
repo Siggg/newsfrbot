f1b57c68710a272a3d653292e3423750ca8755a9
Yves Quemener <Iv@laptop>
2012-10-10 12:09:15 +1100

    Added feeds for lemonde and lefigaro

--- rue89bot.py
+++ rue89bot.py
@@ -19,29 +19,32 @@ reddit.login(user, passwd)
 already_published = cPickle.load(open("already_published","rb"))
 
 while True:
-    d = feedparser.parse('http://rue89.feedsportal.com/c/33822/f/608948/index.rss')
+    rue89feed = feedparser.parse('http://rue89.feedsportal.com/c/33822/f/608948/index.rss')
+    lemondefeed = feedparser.parse('http://www.lemonde.fr/rss/une.xml')
+    lefigarofeed = feedparser.parse('http://rss.lefigaro.fr/lefigaro/laune?format=xml')
 
-    for e in d["entries"]:
-        if not e["link"] in already_published:
-            try:
-                print asctime(), "Publishing :", e["title"]
-                reddit.submit('rue89', e["title"], url=e['link'])
-                sleep(10) # To comply with reddit's policy : no more than 0.5 req/sec
-                already_published.add(e["link"])
-                cPickle.dump(already_published,open("already_published","w"))
-            except praw.errors.APIException as ex:
-                if ex.error_type=='ALREADY_SUB':
+    for d in [('rue89', rue89feed), ('lemonde', lemondefeed), ('lefigaro', lefigarofeed)]:
+        for e in d[1]["entries"]:
+            if not e["link"] in already_published:
+                try:
+                    print asctime(), "Publishing on",d[0],":", e["title"]
+                    reddit.submit(d[0], e["title"], url=e['link'])
+                    sleep(10) # To comply with reddit's policy : no more than 0.5 req/sec
                     already_published.add(e["link"])
-                    print "Already published :", e["title"]
                     cPickle.dump(already_published,open("already_published","w"))
-                else:
-                    print asctime(),"Exception : Reddit offline ? Retrying in 5 minutes"
-                    traceback.print_stack()
-                    print ex.error_type
-                    print ex.message
-                    print ex.field
-                    print ex.response
-                    print "_", sys.exc_info()[0]
-                    sleep(300)
+                except praw.errors.APIException as ex:
+                    if ex.error_type=='ALREADY_SUB':
+                        already_published.add(e["link"])
+                        print "Already published :", e["title"]
+                        cPickle.dump(already_published,open("already_published","w"))
+                    else:
+                        print asctime(),"Exception : Reddit offline ? Retrying in 5 minutes"
+                        traceback.print_stack()
+                        print ex.error_type
+                        print ex.message
+                        print ex.field
+                        print ex.response
+                        print "_", sys.exc_info()[0]
+                        sleep(300)
     sleep(300)
 
