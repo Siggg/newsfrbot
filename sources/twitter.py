f8ab78cec00c6766b7f39637ece5c86fb4950a3d
Siggg <sig@akasig.org>
2016-06-08 14:50:54 +0200

    Update twitter.py
    minor fixes

--- sources/twitter.py
+++ sources/twitter.py
@@ -1,4 +1,4 @@
-﻿# This Python file uses the following encoding: utf-8
+# This Python file uses the following encoding: utf-8
 #
 # This file is part of rss2reddit_using_newsfrbot.
 # 
@@ -28,56 +28,62 @@ import traceback, sys
 # How many users from the twitter lists must mention a link before it is
 # retained as a link worth mentioning
 
-TWITTOS_THRESHOLD = 3
+TWITTOS_THRESHOLD = 4
 
-# Which search queries to you want the bot to monitor for news your lists
+# Which search queries do you want the bot to monitor for news your lists
 # members may retweet ?
+# A previous version of this bot used queries but due to a change in Twitter
+# API, it now has to use Twitter widgets. First create a widget on your twitter
+# account. Your widget will contain the desired search query. Then copy the ID
+# of this Twitter widget and paste it in the list below. The ID of the widget
+# is visible in its  URL. So you should set one widget per search query.
 
-queries = [ 'edtech+lang%3Afr',
-            'education+numerique+lang%3Afr',
-            'formation+numerique+lang%3Afr',
-            'apprendre+numerique+lang%3Afr',
-            'pedagogie+numerique+lang%3Afr',
-            'enseignement+numerique+lang%3Afr',
-            'learning+numerique+lang%3Afr' ]
+searchWidgets = ['621370754232221696',  # edtech lang:fr
+                '621372549700562945',   # education numerique lang:fr
+                '621372917851357184',   # formation numerique lang:fr
+                '621373130078990336',   # apprendre numerique lang:fr
+                '621373528332439552',   # pedagogie numerique lang:fr
+                '621373686512226304',   # enseignement numerique lang:fr
+                '621373890372218880',   # learning numerique lang:fr
+                '621374257625452544']   # éducation numérique
 
 # Which twitter lists will trigger publication by the bot whenever enough
 # of their members retweet some link ?
+# Same as above for lists. One widget to be created by list. Then paste their IDs below.
+# Which Twitter widgets correspond to lists you want to monitor?
 
-lists = [ 'petermortimer/di',
-          'siggg/PedagoRique',
-          'cnedinnovation/di-cned' ]
+listWidgets = ['460403989213757440']    # siggg/pedagorique
 
 # CAUTION : you MUST follow the instructions on labnol.org so that you get
 # YOUR OWN myGoogleScriptID. DO NOT USE the one given below.
 #
-#   http://www.labnol.org/internet/twitter-rss-feeds/27931/
+#   http://www.labnol.org/?p=28149
 #
 # These instructions will show you how to generate your own Google Script
-# and let it to parse Twitter pages using your Twitter user account (and not
+# and let it parse Twitter pages using your Twitter user account (and not
 # mine...) and generate RSS feeds in return.
 # Once your own Google Script is generated, copy the long ID string from
 # its URL and paste it as the value of the myGoogleScriptID variable below :
 
-myGoogleScriptID = 'AKfycbzw8ku5gvJKcWFYHOQS_Dv_6cLECkULNOBaJemN9caSQMl6q7E'
+myGoogleScriptID = ''
 
 # That's it for configuration variables.
 
 try:
     indexedTweets = cPickle.load(open("indexedTweets","rb"))
-except IOError:
+except (IOError, EOFError):
     indexedTweets = dict() # indexedTweets = {'someTweetUrl': True}
 
 try:
     tweetsIndexedByLinks = cPickle.load(open("tweetsIndexedByLinks","rb"))
-except IOError:
+except (IOError, EOFError):
     tweetsIndexedByLinks = dict()
     # entry = {'link': 'someTweetUrl', 'title':'someTweetText'}
     # tweetsIndexedByLinks[someLinkUrl] = {someTweetUrl: entry}
 
 try:
     shortLinks = cPickle.load(open("shortLinks","rb"))
-except IOError:
+except (IOError, EOFError):
     shortLinks = dict() # shortLinks["shortUrl"] = "finalUrl"
 
 TEMPORARILY_SKIPS = []
@@ -175,21 +181,31 @@ class Tweet:
 
         """ Instantiates a tweet from an RSS entry representing a tweet. """
 
-        self.entry=entry
+        self.entry = entry
         self.url = entry["link"].strip()
-        self.text = entry["title"]
-        print
-        print self.text.encode("utf8","replace")[:80]
+        textFields = ['summary', 'value', 'title', 'links']
+        self.text = None
+        for textField in textFields:
+            if textField in entry:
+                text = entry[textField]
+                if text not in ['', None]:
+                    self.text = text
+                    break
+        # print self.simpleText()[:79]
         self.userId = urlsplit(self.url).path.split('/')[1]
 
     def alreadyIndexed(self):
 
         result = self.url in [url.strip() for url in indexedTweets.keys()]
         if result == True:
-            print "(already indexed)"
+            pass # print "(already indexed)"
 	else:
-	    print "(never indexed so far)"
+	    # print "(never indexed so far)"
+	    print self.simpleText()[:79]
         return result
+
+    def simpleText(self):
+        return re.sub('<[^<]+?>', '', self.text.encode("utf8","replace"))
 
     def validLinks(self):
 
@@ -197,7 +213,7 @@ class Tweet:
         not worth exploring (invalid URLs, ...) """
 
         links = []
-        for url in re.findall(r'(https?://\S+)', self.text):
+        for url in re.findall(r'"(https?://\S+?)"', self.text):
             url = url.strip()
             if url is None:
                 continue
@@ -207,6 +223,17 @@ class Tweet:
                 continue
             if url[:16] in ["http://paper.li/"]:
                 continue
+            if url[:19] in ["http://twitter.com/", "https://twitter.com"]:
+                continue
+            if url[:21] in ["http://pbs.twimg.com/", "https://pbs.twimg.com"]:
+                continue
+            if url[:21] in ["http://abs.twimg.com/", "https://abs.twimg.com"]:
+                continue
+            if url[:9] in ["http://</", "https://<"]:
+                continue
+            if url[:13] in ["http://www.</", "https://www.<"]:
+                continue
+            print "url: ", url
             links.append(Link(url))
         return links
 
@@ -237,7 +264,7 @@ class Tweet:
         print asctime(), \
               len(self.links), \
               "links indexed in new tweet :", \
-              self.text.encode("utf8","replace")
+              self.simpleText()
         indexedTweets[self.url] = True
         assert(self.alreadyIndexed())
         cPickle.dump(indexedTweets,open("indexedTweets","wb"))
@@ -273,7 +300,7 @@ class Tweet:
                 # At least TWITTOS_THRESHOLD twitter users have twitted
                 # about this URL.
                 for convergingTweet in convergingTweets:
-                    print asctime(), "about to publish tweet", convergingTweet.text.encode("utf8","replace")
+                    print asctime(), "about to publish tweet", convergingTweet.simpleText()
                     linksToPublish.append((link,convergingTweet))
         return linksToPublish
 
@@ -282,13 +309,11 @@ def get():
 
     scriptPrefix = 'https://script.google.com/macros/s/' \
                     + myGoogleScriptID \
-                    + '/exec?action='
+                    + '/exec?'
     
     nonListFeeds = [ scriptPrefix
-                    + 'search&q='
-                    + query
-                    + '+include%3Aretweets&src=typd'
-                    for query in queries ]
+                    + searchWidgetID
+                    for searchWidgetID in searchWidgets ]
     
     # First, we'll extract links from non-list tweets.
     # Then, we'll parse list tweets and check the links they include.
@@ -296,6 +321,7 @@ def get():
     # Let's access and parse these feeds, and extract their entries
     entries = []
     for feedURL in nonListFeeds:
+        # print "non-list feedURL: ", feedURL
         sleep(2)
         feed = feedparser.parse(feedURL)
         entries += feed.entries
@@ -310,10 +336,9 @@ def get():
     # Now let's examine tweets from the lists and check their links
                     
     listFeeds = [ scriptPrefix
-                 + 'list&q='
-                 + l
-                 for l in lists ]
-    
+                 + listWidgetID
+                 for listWidgetID in listWidgets ]
+
     listEntries = []
     for feedURL in listFeeds:
         sleep(2)
